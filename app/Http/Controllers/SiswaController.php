e3261a6881edcee9d34e45b3136411ddbb7fbbe1
FakhriSyifaurrahman <fakhrisyifaurrahman@gmail.com>
2018-12-12 14:36:07 +0700

    CRUD dengan Laravel 5.6

---
+++ app/Http/Controllers/SiswaController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Siswa;
+use Illuminate\Http\Request;
+
+class SiswaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+    // Tampil data
+        $data = Siswa::all();
+        return view('siswa.index', compact('data'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('siswa.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = new Siswa();
+        $data->nama = $request->nama;
+        $data->alamat = $request->alamat;
+        $data->email = $request->email;
+        $data->save();
+        return redirect()->route('siswa.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        // Detail Siswa
+        $data = Siswa::find($id);
+        return view('siswa.lihat', ['data' => $data]);
+        
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        // Form Edit Siswa
+        $data = Siswa::where('id', $id)->get();
+        return view('siswa.edit', compact('data'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        // Simpan Edit Siswa
+        $data = Siswa::where('id', $id)->first();
+        $data->nama = $request->nama;
+        $data->alamat = $request->alamat;
+        $data->email = $request->email;
+        $data->save();
+        return redirect()->route('siswa.index')->with('alert-success', 'Data berhasil diubah!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+         // delete
+    $data = Siswa::where('id', $id)->first();
+    $data->delete();
+    return redirect()->route('siswa.index');
+    }
+}
---
+++ resources/views/siswa/index.blade.php
@@ -0,0 +1,36 @@
+<a href="{{url('/siswa/create')}}">Tambah Data</a>
+
+<table>
+<thead>
+  <tr>
+    <th>No.</th>
+    <th>Nama</th>
+    <th>Email</th>
+    <th>Alamat</th>
+    <th>Options</th>
+  </tr>
+</thead>
+<tbody>
+  @php
+    $no = 1;
+  @endphp
+  @foreach($data as $items)
+  <tr>
+    <td>{{ $no++ }}</td>
+    <td>{{ $items->nama }}</td>
+    <td>{{ $items->email }}</td>
+    <td>{{ $items->alamat }}</td>
+    <td>
+    <form action="{{ route('siswa.destroy', $items->id) }}" method="post">
+            {{ csrf_field() }}
+            {{ method_field('DELETE') }}
+            <a href="{{ route('siswa.show',$items->id) }}">Lihat</a>
+            <a type="submit" href="{{ route('siswa.edit',$items->id) }}">Edit</a>
+            <button type="submit" onclick="return confirm('Yakin ingin menghapus data?')">Delete</button>
+        </form>
+    </td>
+  </tr>
+  @endforeach
+</tbody>
+</table>
+
